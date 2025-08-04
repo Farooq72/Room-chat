80d3704f77db342f77c4f9f6c144294ddb15d82c
Farooq Azeem <125481539+Farooq72@users.noreply.github.com>
2025-08-04 18:54:01 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -64,35 +64,5 @@ wss.on("connection", (socket) => {
     });
 });
 
-
-// wss.on("connection",(socket)=>{
-
-//     socket.on("message",(message)=>{
-//         const parsedMessage = JSON.parse(message.toString());
-//         if(parsedMessage.type == "join" ){
-//             allSockets.push({
-//                 socket,
-//                 room:parsedMessage.payload.roomId,
-//                 username : parsedMessage.payload.username
-//             })
-//             console.log(parsedMessage);
-//         }
-//         if(parsedMessage.type == "chat"){
-//             const currentUser = allSockets.find((x) => x.socket === socket);
-//             const currentUserRoom = currentUser?.room;
-            
-//             allSockets.forEach((s) => {
-//                 if (s.room === currentUserRoom && s.socket !== socket) {
-//                     s.socket.send(JSON.stringify({
-//                         type: "chat",
-//                         payload: {
-//                             message: parsedMessage.payload.message,
-//                             username: parsedMessage.payload.username
-//                         }
-//                     }));
-//                 }
-//             });
-//         }
-//     })
    
 // });
